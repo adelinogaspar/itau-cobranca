64626be1df805c6e6efec3380186892a491609c1
Gaspar <adelino.gaspar@b2wdigital.com>
2021-08-02 19:24:25 -0300

    multiplas filas (uma por canal)

--- itau-cobranca/src/main/java/com/gaspar/cobranca/config/RabbitmqConfig.java
+++ itau-cobranca/src/main/java/com/gaspar/cobranca/config/RabbitmqConfig.java
@@ -11,11 +11,52 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitmqConfig {
 
-    public static final String QUEUE_NOTIFICA_COBRANCA = "queue.notifica.cobranca";
+    public static final String QUEUE_NOTIFICA_COBRANCA_SMS = "queue.notifica.cobranca.sms";
+    public static final String QUEUE_NOTIFICA_COBRANCA_EMAIL = "queue.notifica.cobranca.email";
+    public static final String QUEUE_NOTIFICA_COBRANCA_TELEGRAM = "queue.notifica.cobranca.telegram";
 
     @Bean
-    Queue queueNotificaCobranca() {
-        return new Queue(QUEUE_NOTIFICA_COBRANCA, true);
+    public DirectExchange direct() {
+        return new DirectExchange("cobranca.direct");
+    }
+
+    @Bean
+    Queue queueNotificaCobrancaSms() {
+        return new Queue(QUEUE_NOTIFICA_COBRANCA_SMS, true);
+    }
+
+    @Bean
+    public Binding bindingSms(DirectExchange direct,
+                             Queue queueNotificaCobrancaSms) {
+        return BindingBuilder.bind(queueNotificaCobrancaSms)
+                .to(direct)
+                .with("sms");
+    }
+
+    @Bean
+    Queue queueNotificaCobrancaTelegram() {
+        return new Queue(QUEUE_NOTIFICA_COBRANCA_TELEGRAM, true);
+    }
+
+    @Bean
+    public Binding bindingTelegram(DirectExchange direct,
+                              Queue queueNotificaCobrancaTelegram) {
+        return BindingBuilder.bind(queueNotificaCobrancaTelegram)
+                .to(direct)
+                .with("telegram");
+    }
+
+    @Bean
+    Queue queueNotificaCobrancaEmail() {
+        return new Queue(QUEUE_NOTIFICA_COBRANCA_EMAIL, true);
+    }
+
+    @Bean
+    public Binding bindingEmail(DirectExchange direct,
+                                Queue queueNotificaCobrancaEmail) {
+        return BindingBuilder.bind(queueNotificaCobrancaEmail)
+                .to(direct)
+                .with("email");
     }
 
     @Bean
--- itau-cobranca/src/main/java/com/gaspar/cobranca/service/QueueService.java
+++ itau-cobranca/src/main/java/com/gaspar/cobranca/service/QueueService.java
@@ -2,6 +2,7 @@ package com.gaspar.cobranca.service;
 
 import com.gaspar.cobranca.config.RabbitmqConfig;
 import com.gaspar.cobranca.entity.Cobranca;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,12 @@ public class QueueService {
     @Autowired
     private RabbitTemplate template;
 
+    @Autowired
+    private DirectExchange direct;
+
     public void enqueue(Cobranca cobranca) {
-        template.convertAndSend(RabbitmqConfig.QUEUE_NOTIFICA_COBRANCA, cobranca);
+        template.convertAndSend(RabbitmqConfig.QUEUE_NOTIFICA_COBRANCA_SMS, cobranca);
+        template.convertAndSend(RabbitmqConfig.QUEUE_NOTIFICA_COBRANCA_EMAIL, cobranca);
+        template.convertAndSend(RabbitmqConfig.QUEUE_NOTIFICA_COBRANCA_TELEGRAM, cobranca);
     }
 }
